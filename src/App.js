140c860148c170e8aec114db1081c4c4175a55e8
Kadek Surya Mahardika <kadeksuryam@gmail.com>
2021-04-13 07:19:37 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Chat } from '@progress/kendo-react-conversational-ui';
+import '@progress/kendo-theme-material/dist/all.css';
+import './app.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.user = {
+          id: 1,
+          avatarUrl: "https://via.placeholder.com/24/008000/008000.png"
+      };
+      this.bot = { id: 0 };
+      this.state = {
+          messages: [
+              {
+                  author: this.bot,
+                  suggestedActions: [
+                      {
+                          type: 'reply',
+                          value: 'Oh, really?'
+                      }, {
+                          type: 'reply',
+                          value: 'Thanks, but this is boring.'
+                      }
+                  ],
+                  timestamp: new Date(),
+                  text: "Hello, this is a demo bot. I don't do much, but I can count symbols!"
+              }
+          ]
+      };
+  }
+
+  addNewMessage = (event) => {
+      let botResponce = Object.assign({}, event.message);
+      botResponce.text = this.countReplayLength(event.message.text);
+      botResponce.author = this.bot;
+      this.setState((prevState) => ({
+          messages: [
+              ...prevState.messages,
+              event.message
+          ]
+      }));
+      setTimeout(() => {
+          this.setState(prevState => ({
+              messages: [
+                  ...prevState.messages,
+                  botResponce
+              ]
+          }));
+      }, 1000);
+  };
+
+  countReplayLength = (question) => {
+      let length = question.length;
+      let answer = question + " contains exactly " + length + " symbols.";
+      return answer;
+  }
+
+  render() {
+      return (
+        <div>
+          <Chat className="chat-container" user={this.user}
+            messages={this.state.messages}
+            onMessageSend={this.addNewMessage}
+            placeholder={"Type a message..."}/>
+        </div>
+      );
+  }
 }
 
 export default App;
