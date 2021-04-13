83b3c5aa3546d75c09c8481caa87beb7247ea79a
Kadek Surya Mahardika <kadeksuryam@gmail.com>
2021-04-13 09:31:47 +0700

    initial commit for heroku

--- api/api.py
+++ api/api.py
@@ -1,8 +1,16 @@
 import time
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/api/time', methods=['GET'])
 def get_current_time():
     return {'time': time.time()}
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
